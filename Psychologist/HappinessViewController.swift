b247e27155d981f9ee7a77639a54b0937262ca21
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-14 21:25:01 +0200

    defined model

--- Psychologist/HappinessViewController.swift
+++ Psychologist/HappinessViewController.swift
@@ -2,5 +2,13 @@ import UIKit
 
 class HappinessViewController: UIViewController
 {
+    let verySad: Int = 0
+    let ecstatic: Int = 100
+    var happiness: Int = 25 {
+        didSet {
+            happiness = min(max(happiness, verySad), ecstatic)
+        }
+    }
+    
 }
 
