dfabc02d141202be0737719395d9d3dfc6f5fd70
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-14 22:28:34 +0200

    add PsychologistViewController

---
+++ Psychologist/PsychologistViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PsychologistViewController.swift
+//  Psychologist
+//
+//  Created by Martinho on 14/04/15.
+//  Copyright (c) 2015 Martinho. All rights reserved.
+//
+
+import UIKit
+
+class PsychologistViewController: UIViewController {
+
+}
