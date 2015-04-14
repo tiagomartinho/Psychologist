8fa0ee5ac7b005fcf62edfe4ccda05eed54d64f8
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-14 21:48:25 +0200

    update ui when model changes correct interpretation of the model

--- Psychologist/HappinessViewController.swift
+++ Psychologist/HappinessViewController.swift
@@ -4,15 +4,20 @@ class HappinessViewController: UIViewController, FaceViewDataSource
 {
     let verySad: Int = 0
     let ecstatic: Int = 100
-    var happiness: Int = 25 {
+    var happiness: Int = 50 {
         didSet {
             happiness = min(max(happiness, verySad), ecstatic)
+            updateUI()
         }
+    }
+    
+    func updateUI() {
+        faceView.setNeedsDisplay()
     }
     
     let frown: Double = -1.0
     let smile: Double = +1.0
-    var m:Double { return (frown-smile)/Double(ecstatic-verySad) }
+    var m:Double { return (frown-smile)/Double(verySad-ecstatic) }
     var b:Double { return frown - Double(verySad) * m}
     func smilinessForFaceView(sender: FaceView) -> Double? {
         return m*Double(happiness)+b
