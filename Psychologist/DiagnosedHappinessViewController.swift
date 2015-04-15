8e0fe16f43f1abaf56f9d4e6ec59569c1ecbb556
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-15 21:33:00 +0200

    set text view when showing popover

--- Psychologist/DiagnosedHappinessViewController.swift
+++ Psychologist/DiagnosedHappinessViewController.swift
@@ -2,6 +2,21 @@ import UIKit
 
 class DiagnosedHappinessViewController : HappinessViewController,UIPopoverPresentationControllerDelegate
 {
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            switch identifier {
+            case "Show Diagnostic History":
+                if let tvc = segue.destinationViewController as? TextViewController {
+                    if let ppc = tvc.popoverPresentationController {
+                        ppc.delegate = self
+                    }
+                    tvc.text = "diagnosticHistory"
+                }
+            default: break
+            }
+        }
+    }
+    
     func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
         return UIModalPresentationStyle.None
     }
