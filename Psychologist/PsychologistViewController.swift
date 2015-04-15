5b2a4739c5bb428881df69981450b78a8e7a67de
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-15 21:43:37 +0200

    organised project and removed copyright

--- Psychologist/PsychologistViewController.swift
+++ Psychologist/PsychologistViewController.swift
@@ -1,11 +1,3 @@
-//
-//  PsychologistViewController.swift
-//  Psychologist
-//
-//  Created by Martinho on 14/04/15.
-//  Copyright (c) 2015 Martinho. All rights reserved.
-//
-
 import UIKit
 
 class PsychologistViewController: UIViewController
